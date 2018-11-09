8e57ea619832f8cbe244ad706826e31662561b7d
Timmy <timgabrielnguyen@gmail.com>
2018-11-09 13:31:18 -0800

    First Commit

---
+++ themes/camp/js/custom.js
@@ -0,0 +1,16 @@
+(function($) {
+
+    $(document).ready(function() {
+
+    
+    //your code here
+    $('body').append('Maggie Cheung');//just testing 
+
+    //focus and learn will be used for toggling search metho
+    });
+})(jQuery);
+
+//IIFE
+
+//immediately invoked function expression
+
